b566b7fd2de8146db70df81b325067701d439f02
flybit <alphan@bu.edu>
2018-09-07 16:42:18 -0400

    Add google maps, make left-menu work, style changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import POIList from './POIList';
-
-import logo from './logo.svg';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 import './App.css';
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+));
+
 
 class App extends Component {
   state = {
@@ -10,6 +20,7 @@ class App extends Component {
       {id: 1, name: 'foo'},
       {id: 2, name: 'bar'},
     ],
+    showList: false,
     query: ''
   }
 
@@ -18,11 +29,28 @@ class App extends Component {
   }
 
   render() {
-    const { poi, query } = this.state;
+    const { poi, showList, query } = this.state;
     let poiFiltered = {}
     return (
-      <div>
-        <POIList poi={poi} query={query} onQueryChange={this.handleQueryChange}/>
+      <div className="main-container">
+
+        <POIList poi={poi} showList={showList} query={query} onQueryChange={this.handleQueryChange}/>
+
+        <div className={classNames("right-part", {"left-shown": showList})}>
+          <div className="top-menu" onClick={() => this.setState({showList: !showList})}>
+            <i className="material-icons">menu</i>
+          </div>
+
+
+          <MyMapComponent isMarkerShown
+              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCn9Mj7GvNhbxo-yfh3gfIVbiCRtKYehMs"
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: `calc(100% - 25px)` }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+              />
+
+          </div>
+
       </div>
     );
   }
--- src/POIList.js
+++ src/POIList.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 
 
 class POIList extends Component {
 
   render() {
-    const { poi, query, onQueryChange } = this.props;
+    const { poi, showList, query, onQueryChange } = this.props;
     return (
-      <div className="list">
-        <div className="filter">
+      <div className={classNames('left-part', {"left-shown": showList})}>
+        <div className="filter-container">
           <input className="filter" type="text" placeholder="Type here to filter locations" value={query} onChange={(e) => onQueryChange(e.target.value)}/>
-          <ol>
-            {poi.map(p => (<li key={p.id}>{p.name}</li>))}
+        </div>
+        <div className="list-container">
+          <ol className="list">
+            {poi.map(p => (<li className="list-item" key={p.id}>{p.name}</li>))}
           </ol>
         </div>
       </div>
